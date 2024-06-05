0b9841182244a3f39edf0f8c9c5db2912dfdc499
Boxy <nozakisola@gmail.com>
2024-06-04 21:08:43 -0500

    Update desktop

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 1280,
-      height: 720
+      height: 720,
+      autoHideMenuBar: true
     })
   
     win.loadFile('index.html')
