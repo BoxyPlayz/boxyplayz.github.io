8daf9bfc0772bb97ef4f0a97d97fe7b3acfdf39d
Boxy <nozakisola@gmail.com>
2025-05-23 11:16:15 -0500

    Legacy Support

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
-import { resolve } from 'path'
+import { resolve } from "path";
 
 export default {
-  root: resolve(__dirname, 'src'),
+  root: resolve(__dirname, "src"),
   build: {
-    outDir: '../dist'
+    outDir: "../dist",
+    minify: "esbuild",
+    sourcemap: false,
+    cssCodeSplit: true,
   },
   server: {
-    port: 8080
+    port: 8080,
   },
   preview: {
-    port: 8080
-  }
-}
+    port: 8080,
+  },
+  plugins: [
+    legacy({
+      targets: ["IE 11, Firefox ESR"],
+      modernTargets: [
+        "chrome >= 70",
+        "firefox >= 70",
+        "safari >= 11",
+        "edge >= 18",
+      ],
+      additionalLegacyPolyfills: true,
+    }),
+  ],
+};
